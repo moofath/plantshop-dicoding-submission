7d96c5ff52b9e963a3afa9130178e71632ada517
Muhammad Fathur Rohman <36975265+fathurarj12@users.noreply.github.com>
2022-07-19 14:21:23 +0700

    push fix

--- js/script.js
+++ js/script.js
@@ -1,19 +1,48 @@
 $(document).ready(function(){
-    $('#menu').click(function(){
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
+    $('#menu').click(function(){ //id menu saat di klik / tekan
+        $(this).toggleClass('fa-times'); // merubah ikon menubar menjadi tanda silang saat id menu di tekan
+        $('.navbar').toggleClass('nav-toggle'); //menampilkan isi navbar saat diklik id menu pada saat mode responsive
     });
 
-    $(window).on('load scroll',function(){
-        $('#menu').removeClass('fa-times');
-        $('.navbar').removeClass('nav-toggle');
-
-        // let bcolor = 
-
-        if($(window).scrollTop() > 0){
-            $('header').addClass('sticky');
+    // fungsi saat melakukan scroll halaman
+    $(window).on('load scroll',function(){ 
+        if($(window).scrollTop() > 0){ //kondisi jika halaman melakukan scroll  dengan nilai lebih dari 0 (top)/ kebawah
+            $('header').addClass('sticky'); //jika terpenuhi maka header akan di kasih kelas sticky
         } else {
-            $('header').removeClass('sticky');
+            $('header').removeClass('sticky'); //jika tidak terpenuhi maka hapus kelas sticky dari header
+        }
+    });
+
+    //fungsi dibawah untuk menampilkan carousel
+    $(".carousel").owlCarousel({
+        margin: 500,
+        loop: true, //perulangan bernilai true (melakuakan perulangan dari awal card sampai akhir card dan kembali lagi ke awal card )
+        autoplay: true, //slider akan berjalan sendirinya atau autoplay tanpa harus di geser
+        autoplayTimeout: 2000, //waktu slider berjalan selama 2 detik
+        autoplayHoverpause: true,
+        responsive: {
+            0:{
+                items:1,
+                nav: false
+            },
+            600:{
+                items:1,
+                nav: false
+            },
+            768:{
+                items:1,
+                nav: false
+            },
+            1000:{
+                margin:10,
+                items:2,
+                nav: false
+            }
         }
     });
 });
+
+
+function btnDropdown(){
+    document.getElementById("dropdown").classList.toggle("show"); //menampilkan content dropdown ketika button di klik
+}
